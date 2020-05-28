68903d0fb10afb362f67f9eb9289258e3cef4db6
Artem <56354347+Artem-Rysich@users.noreply.github.com>
2020-05-28 14:08:35 +0300

    Add files via upload

--- task-6.js
+++ task-6.js
@@ -1,11 +1,13 @@
 'use strict';
 const input = document.getElementById('validation-input');
-const handlerInput = event => {
+input.addEventListener('change', handlerInput);
+function handlerInput (event)  {
   const validLength = Number(input.getAttribute('data-length'));
   if (event.currentTarget.value.length !== validLength) {
     input.classList.add('invalid');
+    input.classList.remove('valid');
   } else {
     input.classList.add('valid');
+    input.classList.remove('invalid');
   }
 };
-input.addEventListener('change', handlerInput);
